11aff3c1c563193b2f5809002a32f1c7a478b09a
Artem Rudenko <artem.rudenko@gmail.com>
2023-01-21 22:38:35 +0300

    Add asteroid.

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField] private float _speed = 10.0f;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * _speed;
+
+        var indicator = IndicatorManager.instance.AddIndicator(gameObject, Color.red);
+    }
+}
